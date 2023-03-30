751f43cc49ea8bca46a0e87a8597b63f74b72f76
Yulia Trubacheva <yulitrub@gmail.com>
2023-03-30 21:00:25 +0100

    finish header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,31 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Header.scss";
 
 const Header = () => {
   return (
     <header className="header">
       <section className="header__wrap">
-        <Link to="/home">
-          <img src="" alt="logo" className="header__logo" />
+        <Link to="/">
+          <img
+            src="https://www.britishairways.com/cms/global/assets/images/site/brand/ba_masterbrand_positive.svg"
+            alt="logo"
+            className="header__logo"
+          />
         </Link>
 
         <div className="header__icons">
-          <img src="" alt="search" className="header__search" />
+          <img
+            src="https://www.britishairways.com/cms/global/assets/images/site/icon/header_search.svg"
+            alt="search"
+            className="header__search"
+          />
           <div className="header__profile-wrap">
-            <img src="" alt="" className="header__profile-icon" />
+            <img
+              src="https://www.britishairways.com/cms/global/assets/images/site/icon/header_login.svg"
+              alt="lprofile"
+              className="header__profile-icon"
+            />
             <p className="header__login">LOG IN</p>
           </div>
         </div>
