e69e9f84f16f993d441854731c2764b15dfb2b74
Yulia Trubacheva <yulitrub@gmail.com>
2023-03-31 12:35:45 +0100

    economy class

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -2,10 +2,18 @@ import React, { useState } from "react";
 import line from "../../assets/images/line.png";
 import GallerySlider from "../GallerySlider/GallerySlider";
 import "./Modal.scss";
+//images for slider
 import img1 from "../../assets/images/img_1.png";
 import img2 from "../../assets/images/img_2.png";
 import img3 from "../../assets/images/img_3.png";
 import img4 from "../../assets/images/img_4.png";
+
+import ec_img1 from "../../assets/images/ec_img1.png";
+import ec_img2 from "../../assets/images/ec_img2.png";
+import ec_img3 from "../../assets/images/ec_img3.png";
+import ec_img4 from "../../assets/images/ec_img4.png";
+
+//icons
 import baggage from "../../assets/icons/baggage.png";
 import point from "../../assets/icons/point.svg";
 import drink from "../../assets/icons/drink.svg";
@@ -27,6 +35,8 @@ import { Link } from "react-router-dom";
 const Modal = ({ isFirstHovered, setIsFirstHovered }) => {
   const [showPreFlight, setShowPreFlight] = useState(true);
   const [showInFlight, setShowInFlight] = useState(false);
+
+  const [isEconomy, setIsEconomy] = useState(false);
   const [isPriority, setIsPriority] = useState(true);
 
   const toggleRightHandle = () => {
@@ -38,11 +48,17 @@ const Modal = ({ isFirstHovered, setIsFirstHovered }) => {
     setShowInFlight(false);
     setShowPreFlight(true);
   };
-  const handleClass = () => {
-    setIsPriority(!isPriority);
+  const handleFirstClass = () => {
+    setIsEconomy(false);
+    setIsPriority(true);
+  };
+  const handleEconomyClass = () => {
+    setIsPriority(false);
+    setIsEconomy(true);
   };
 
   const imagesFirst = [img1, img2, img3, img4];
+  const imagesEconomy = [ec_img1, ec_img2, ec_img3, ec_img4];
 
   return (
     <section className="modal">
@@ -66,7 +82,7 @@ const Modal = ({ isFirstHovered, setIsFirstHovered }) => {
             )}
 
             <Link
-              onClick={handleClass}
+              onClick={handleFirstClass}
               className="classes__button classes__button--first"
               onMouseEnter={() => setIsFirstHovered(true)}
               onMouseLeave={() => setIsFirstHovered(false)}
@@ -93,14 +109,14 @@ const Modal = ({ isFirstHovered, setIsFirstHovered }) => {
           </Link>
           <Link
             className="classes__button classes__button--economy"
-            onClick={handleClass}
+            onClick={handleEconomyClass}
           >
             <p className="classes__class">Economy</p>
             <p className="classes__price">$766</p>
           </Link>
         </div>
       </div>
-      <div className={`content ${isPriority ? "" : "content--hidden"}`}>
+      <div className={`content ${isEconomy ? "content--hidden" : ""}`}>
         <div className="content__info">
           <ul className="options">
             <li
@@ -152,17 +168,51 @@ const Modal = ({ isFirstHovered, setIsFirstHovered }) => {
           </ul>
           <ul className={`features ${showInFlight ? "" : "features--hidden"}`}>
             <li className="features__item">
+              <img src={macbook} alt="laptop" className="features__icon" />
+              <p className="features__text">
+                Work or sleep from your seat with a fully lie-flat bed
+              </p>
+            </li>
+            <li className="features__item">
               <img src={door} alt="door" className="features__icon" />
               <p className="features__text">
-                Take your pick from the hundreds of latest films, television
-                programmes, audio books and games{" "}
+                Club Suite: window and aisle access from all suites, with a
+                sliding privacy door and generous storage{" "}
+              </p>
+            </li>
+            <li className="features__item">
+              <img src={bottle} alt="bottle" className="features__icon" />
+              <p className="features__text">
+                A bespoke selection of high-quality wines, chosen by our very
+                own Master of Wine
+              </p>
+            </li>
+            <li className="features__item">
+              <img src={user} alt="somelier" className="features__icon" />
+              <p className="features__text">
+                Sommelier trained crew & trained to have every meal placed
+                perfectly
               </p>
             </li>
             <li className="features__item">
               <img src={restaurant} alt="food" className="features__icon" />
               <p className="features__text">
-                Enjoy a starter, main and dessert, with a choice of hot, cold
-                and alcoholic drinks
+                Premium multi-course dining experience, with express and sleeper
+                options to fit your needs
+              </p>
+            </li>
+            <li className="features__item">
+              <img src={cake} alt="cake" className="features__icon" />
+              <p className="features__text">
+                Access to the Club Kitchen onboard, for a range of on-demand
+                snacks and refreshments
+              </p>
+            </li>
+            <li className="features__item">
+              <img src={sanitizer} alt="sanitizer" className="features__icon" />
+              <p className="features__text">
+                Amenity kit, bedding and pillow designed for you by The White
+                Company
               </p>
             </li>
             <li className="features__item">
@@ -177,12 +227,20 @@ const Modal = ({ isFirstHovered, setIsFirstHovered }) => {
                 Priority rebooking and premium hotel offered when disrupted
               </p>
             </li>
+            <li className="features__item">
+              <img src={cart} alt="cart" className="features__icon" />
+              <p className="features__text">
+                Priority baggage arrival at T5, for a stress-free end to your
+                journey
+              </p>
+            </li>
           </ul>
         </div>
         <div className="gallery">
           <GallerySlider images={imagesFirst} />
         </div>
       </div>
+
       <div className={`content ${isPriority ? "content--hidden" : ""}`}>
         <div className="content__info">
           <ul className="options">
@@ -215,18 +273,17 @@ const Modal = ({ isFirstHovered, setIsFirstHovered }) => {
             <li className="features__item">
               <img src={door} alt="door" className="features__icon" />
               <p className="features__text">
-                Club Suite: window and aisle access from all suites, with a
-                sliding privacy door and generous storage{" "}
+                Take your pick from the hundreds of latest films, television
+                programmes, audio books and games{" "}
               </p>
             </li>
             <li className="features__item">
               <img src={restaurant} alt="food" className="features__icon" />
               <p className="features__text">
-                Premium multi-course dining experience, with express and sleeper
-                options to fit your needs
+                Enjoy a starter, main and dessert, with a choice of hot, cold
+                and alcoholic drinks
               </p>
             </li>
-
             <li className="features__item">
               <img src={briefcase} alt="briefcase" className="features__icon" />
               <p className="features__text">
@@ -236,7 +293,7 @@ const Modal = ({ isFirstHovered, setIsFirstHovered }) => {
           </ul>
         </div>
         <div className="gallery">
-          {/* <GallerySlider images={imagesEconomy} /> */}
+          <GallerySlider images={imagesEconomy} />
         </div>
       </div>
     </section>
